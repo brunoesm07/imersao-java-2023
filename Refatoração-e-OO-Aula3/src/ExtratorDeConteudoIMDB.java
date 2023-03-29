548e975bc1919f9aee4c7fe34d84e38538152c0d
unknown <brunoesm07@hotmail.com>
2023-03-29 16:11:17 -0300

    Aula 3

---
+++ Refatoração-e-OO-Aula3/src/ExtratorDeConteudoIMDB.java
@@ -0,0 +1,17 @@
+import java.util.List;
+import java.util.Map;
+
+public class ExtratorDeConteudoIMDB implements ExtratorDeConteudo {
+
+    public List<Conteudo> extraiConteudos(String json) {
+
+        // extrair somente os dados de interesse (titulo, poster, classificação)
+        var parser = new JsonParser();
+        List<Map<String, String>> listaDeAtributos = parser.parse(json);
+
+        return listaDeAtributos.stream()
+            .map(atributos -> new Conteudo(atributos.get("title"), atributos.get("image").replaceAll("(@+) (.*).jpg$", "$1.jpg")))
+            .toList();
+
+    }
+}   
